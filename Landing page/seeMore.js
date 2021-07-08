45652b8a5e826a675ebd293fb5be84ebc7566ccb
Robert <orazurobert@yahoo.com>
2021-07-08 12:16:43 +0100

    Login

--- Landing page/seeMore.js
+++ Landing page/seeMore.js
@@ -1,8 +1,14 @@
+const greeting = document.getElementById("greeting");
+
+if(localStorage.token){
+  greeting.innerHTML= `Hello ${localStorage.getItem('userFirstName')}?<a href="/Login/login.html">Sign out</a>`;
+}
+
 const seeMore = () => {
     const dots = document.getElementById("dots");
     const moreText = document.getElementById("more");
     const btnText = document.getElementById("myBtn");
-  
+
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       btnText.innerHTML = "See more Projects";
