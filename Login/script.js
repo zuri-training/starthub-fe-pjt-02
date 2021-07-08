7d2ee0022951d566e7d4319c73ead7f8b602a499
Robert <orazurobert@yahoo.com>
2021-07-08 09:07:17 +0100

    login linked to backend

--- Login/script.js
+++ Login/script.js
@@ -1,8 +1,31 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-        x.type = "text";
+const form = document.getElementById("loginForm");
+
+const showPassword = () => {
+    var passwordInput = document.getElementById("passwordValue");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
     } else {
-        x.type = "password";
+        passwordInput.type = "password";
     }
 }
+
+const login = (e) => {
+    e.preventDefault()
+    axios.post('https://damp-fortress-19499.herokuapp.com/api/v1/auth/sign-in', {
+            email: e.target.email.value,
+            password: e.target.password.value
+        })
+        .then(function(response) {
+            localStorage.setItem("userFirstName", response.data.data.firstName);
+            localStorage.setItem("userLastName", response.data.data.lastName)
+            localStorage.setItem("userEmail", response.data.data.email)
+            localStorage.setItem("token", response.data.data.token)
+        })
+        .then(function() {
+            window.location.href = "http://127.0.0.1:5500/Landing%20page/index.html";
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+form.addEventListener('submit', login);
